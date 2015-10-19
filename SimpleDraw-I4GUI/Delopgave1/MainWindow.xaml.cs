d51d12ef2f9c4883740b8c7ebaab4912ef07c6e8
Alex Carstensen <matthacker.alex@gmail.com>
2015-10-19 13:08:14 +0200

    added MainWindow_MouseMove eventhandler

--- SimpleDraw-I4GUI/Delopgave1/MainWindow.xaml.cs
+++ SimpleDraw-I4GUI/Delopgave1/MainWindow.xaml.cs
@@ -12,17 +12,78 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Delopgave1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public enum DrawingTool
+    {
+        Line,
+        Ellipsis,
+        Rectangle
+    };
     public partial class MainWindow : Window
     {
+        private Color _drawColor;
+        private Point _startPos;
+        private Point _currPos;
+        private Shape _currShape;
+        private bool isDrawing = false;
+        private DrawingTool _tool;
+        private DispatcherTimer _timer;
         public MainWindow()
         {
             InitializeComponent();
+          //  KeyUp += new KeyEventHandler(MainWindow_KeyUp);
+          //  MouseDown += new MouseButtonEventHandler(MainWindow_MouseDown);
+         //   MouseUp += new MouseButtonEventHandler(MainWindow_MouseUp);
+            MouseMove += new MouseEventHandler(MainWindow_MouseMove);
+          //  SetColor(Colors.Black);
+        }
+
+        private void SetColor(Color black)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MainWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            _currPos = e.GetPosition(canvas);
+            tbxX.Text = _currPos.X.ToString("F0");
+            tbxY.Text = _currPos.Y.ToString("F0");
+
+            if (!isDrawing) return;
+            if (_tool != DrawingTool.Line)
+            {
+                var width = _currPos.X - _startPos.X;
+                var height = _currPos.Y - _startPos.Y;
+                _currShape.Width = (width > 1 ? width : 2);
+                _currShape.Height = (height > 1 ? height : 2);
+            }
+            else
+            {
+                var l = _currShape as Line;
+                l.X2 = _currPos.X;
+                l.Y2 = _currPos.Y;
+            }
+        }
+
+        private void MainWindow_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MainWindow_KeyUp(object sender, KeyEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
